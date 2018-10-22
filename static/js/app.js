ada672b62ead980a5b56e0382929dce73f564829
Bill Frye <3730304+billfrye@users.noreply.github.com>
2018-10-22 15:42:04 -0500

    Interm save

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,17 @@
+
+from flask import Flask, render_template;
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+app = Flask(__name__)
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template("index.html",sightings = tableData)
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
